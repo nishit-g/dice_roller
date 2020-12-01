0969effb44ab2b4df4cf96345480701b023e958c
nishit-g <nishit.gupta6708@gmail.com>
2020-12-01 13:50:54 +0530

    add project

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.diceroller.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // initialize binding first
+        binding = ActivityMainBinding.inflate(layoutInflater);
+
+        // now set the activity's layout
+        setContentView(binding.root);
+
+        binding.rollButton.text = "Let's Roll"
+
+        // checking the roll button
+        binding.rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val random = Random().nextInt(6) + 1
+        val diceImage = binding.diceImage
+
+        val drawableResource = when (random) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
